5f5eb3c3ddb11a92bcf9fa7d96cd9b4b3ddbaed4
whajun <pypolet@gmail.com>
2025-07-21 15:12:29 +0900

    커맨드는 수정가능
    확장수정가능

---
+++ C/Basic/loop01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void)
+{
+	int i = 100;
+	while(i >= 0)
+	{
+		printf("%d\n", i--);
+		//i -= 1;
+		//i--;
+		
+	}
+	return 0;
+}
